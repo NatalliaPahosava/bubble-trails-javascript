c6c00b9f40329a1d531c41b7b6bc3ef90a82f8b0
NatalliaPahosava <mulato4ka114@gmail.com>
2022-11-27 12:03:43 +0300

    first commit

---
+++ src/bubble-trails.js
@@ -0,0 +1,51 @@
+const container = document.querySelector('#root')
+let currentCount = 0
+const color = [
+  'Aquamarine',
+  'Purple',
+  'Salmon',
+  'Yellow',
+  'Blue',
+  'Red',
+  'Green',
+]
+const createBubble=(posX,posY)=>{
+    const randomColor=color[Math.floor(Math.random()*color.length)]
+
+    const bubble=document.createElement('div')
+    bubble.className="bubble"
+    bubble.style.backgroundColor=randomColor
+    bubble.style.left=`${posX}px`
+    bubble.style.top=`${posY}px`
+
+    container.appendChild(bubble)
+    setTimeout(()=>container.removeChild(bubble),8000)
+}
+
+const updateStats=(element,value)=> document.querySelector(element).innerHTML=value
+
+window.addEventListener('mousemove', 
+(e)=>{
+    createBubble(e.clientX,e.clientY)
+
+    updateStats('.posX',e.clientX)
+    updateStats('.posY',e.clientY)
+    updateStats('.count',`#${currentCount++}`)
+
+}
+)
+window.addEventListener("touchstart", (e) => {
+    [...e.changedTouches].forEach(touch => {
+        createBubble(touch.pageX, touch.pageY)
+        updateStats(".posX", touch.pageX)
+        updateStats(".posY", touch.pageY)
+    })
+})
+
+window.addEventListener("touchmove", (e) => {
+    console.log('move')
+})
+
+window.addEventListener("touchend", (e) => {
+    console.log('end')
+})
